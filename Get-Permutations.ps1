9c1e814d33c3f026ac1105ae64ba86ecb7dc82bd
nathanael.ries <nathanael.ries@gmail.com>
2019-01-08 22:03:02 -0600

    Add files via upload

---
+++ Get-Permutations.ps1
@@ -0,0 +1,17 @@
+﻿function Get-Permutations($text, $NumberOfDigits, $numberOfChars)
+{
+    if ($numberOfDigits -gt 0)
+    {
+        for ($j = 0; $j -lt $numberOfChars; $j++)
+        {
+            Get-Permutations -text ($Text + $j.ToString()) -numberOfDigits ($numberOfDigits - 1) -numberOfChars $numberOfChars
+        }
+    }
+    else 
+    {
+        $result += $text
+    }
+    return $result
+}
+
+Get-Permutations -text "" -NumberOfDigits 3 -NumberOfChars 2
